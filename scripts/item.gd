bbf8eed979fcd693dcddf584201a51437262c253
ApolloSun05 <eugene.ibias@gmail.com>
2025-12-30 01:52:11 +0800

    gnidsnfis

--- scripts/item.gd
+++ scripts/item.gd
@@ -5,13 +5,13 @@ func _get_drag_data(at_position):
 	
 	preview_texture.texture = texture
 	preview_texture.expand_mode = 1
-	preview_texture.size = Vector2(30, 30)
+	preview_texture.size = Vector2(100, 100)
 	
 	var preview = Control.new()
 	preview.add_child(preview_texture)
 	
 	set_drag_preview(preview)
-	return texture
+	return preview_texture.texture
 
 func _can_drop_data(at_position, data):
 	return data is Texture2D
