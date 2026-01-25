b7284c455c5dd320cc52a3d29b2d97fa7741a8df
YUNa <jahitosis1@up.edu.ph>
2026-01-26 01:11:25 +0800

    moved

--- scripts/mailarea.gd
+++ scripts/mailarea.gd
@@ -1,1 +1,20 @@
 extends Node2D
+
+@onready var time_label: Label = $"Timer/Time Label"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Global.time_updated.connect(_update_timer)
+	Global.midnight.connect(
+	func():
+		get_tree().change_scene_to_file("res://scenes/main scenes/delivery.tscn")
+		)
+	Global.start_game()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _update_timer() -> void:
+	var hours: int = int(Global.current_time)
+	var minutes_float = Global.current_time - hours
+	var minutes: int = minutes_float * 60
+	time_label.text = str(hours).pad_zeros(2) + ":" + str(minutes).pad_zeros(2) + " PM" if Global.current_time < Global.end_time else " AM"
+	pass
