3250e97e78d3ef75e38812a6f33c57297196638a
YUNa <jahitosis1@up.edu.ph>
2026-01-26 15:21:30 +0800

    sadsadsadsadsa

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
-
+@onready var credits_ui: CanvasLayer = $CreditsUI
 
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main scenes/mailarea.tscn")
+
+func _on_credits_pressed() -> void:
+	credits_ui.visible = true
+	pass # Replace with function body.
+
+func _on_button_pressed() -> void:
+	credits_ui.visible = false
+	pass # Replace with function body.
